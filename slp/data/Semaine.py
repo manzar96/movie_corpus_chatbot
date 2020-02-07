f6d851c4d8a8a8914a0c71e2b39883c7c9901267
manzar96 <manoszar96@gmail.com>
2020-02-07 21:04:39 +0200

    updated semaine dataloader

--- slp/data/Semaine.py
+++ slp/data/Semaine.py
@@ -3,6 +3,7 @@ import glob
 import os
 import re
 
+
 class SemaineDataset(Dataset):
     def __init__(self, directory, transforms=None, train=True):
 
@@ -10,7 +11,6 @@ class SemaineDataset(Dataset):
 
     def read_data(self, directory):
         path = os.path.join(directory, "Sessions")
-        counter = 0
         for i in range(1,3):
             new_path = os.path.join(path, str(i))
             transcript = glob.glob(os.path.join(new_path,
@@ -19,19 +19,65 @@ class SemaineDataset(Dataset):
                                                      'wordLevel*user'))
             word_level_operator = glob.glob(os.path.join(
                 new_path, 'wordLevel*operator'))
+            arousal_files = glob.glob(os.path.join(new_path, '*DA.txt'))
+
+            line_label_counter = 0
+            if not arousal_files == []:
+                label_file = open(arousal_files[0], "r")
+                labels_lines = label_file.readlines()
+            else:
+                pass
 
             if not transcript == []:
                 with open(transcript[0],"r")as tfile:
+
                     lines = tfile.readlines()
-                    for line in lines:
+                    for index,line in enumerate(lines):
+
                         start_time = line.split("\t")[0].split(" ")[0]
                         end_time = line.split("\t")[0].split(" ")[1]
-                        utt = line.split("\t")[1]
-                        utt = re.sub("\([^)]+\).","",utt)
-                        utt = utt.strip()
-                        print(start_time)
-                        print(end_time)
-                        print(utt)
+                        utt1 = line.split("\t")[1]
+                        utt1 = re.sub("\([^)]+\).","",utt1)
+                        utt1 = utt1.strip()
+
+                        start_min,start_sec,start_ms = start_time.split(":")
+                        start1 = int(start_min)*60+int(
+                            start_sec)+int(start_ms)/1000
+                        end_min,end_sec,end_ms = end_time.split(":")
+                        end1 = int(end_min)*60+int(
+                            end_sec)+int(end_ms)/1000
+                        print(utt1)
+
+                        #stop writing to file as we read all lines!
+                        if index < len(lines)-2:
+                            next_line = lines[index+1]
+                        else:
+                            break
+
+                        start_time = next_line.split("\t")[0].split(" ")[0]
+                        end_time = next_line.split("\t")[0].split(" ")[1]
+                        utt2 = next_line.split("\t")[1]
+                        utt2 = re.sub("\([^)]+\).","",utt2)
+                        utt2 = utt2.strip()
+
+                        start_min,start_sec,start_ms = start_time.split(":")
+                        start2 = int(start_min)*60+int(
+                            start_sec)+int(start_ms)/1000
+                        end_min,end_sec,end_ms = end_time.split(":")
+                        end2 = int(end_min)*60+int(
+                            end_sec)+int(end_ms)/1000
+                        print(utt1)
+
+                        while line_label_counter < len(labels_lines):
+                            label_line = labels_lines[line_label_counter]
+                            time, label = label_line.strip().split(" ")
+                            time = float(time)
+                            if start1+time < end2:
+                                print(time, label)
+
+                            line_label_counter += 1
+
+
 
     def map(self, t):
         self.transforms.append(t)
