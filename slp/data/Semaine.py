39d34f957a59d8ac6ae95a6549b2662b79685a7c
manzar96 <manoszar96@gmail.com>
2020-02-10 20:56:42 +0200

    fixed Semaine wrapper triples only

--- slp/data/Semaine.py
+++ slp/data/Semaine.py
@@ -14,25 +14,41 @@ class SemaineDataset(Dataset):
         path = os.path.join(directory, "Sessions")
         for i in range(1, 129):
             new_path = os.path.join(path, str(i))
-            transcriptions = os.path.join(new_path, 'transcriptions.txt')
+            transcription = os.path.join(new_path, 'transcription.txt')
             arousal_files = glob.glob(os.path.join(new_path, '*DA.txt'))
             valence_files = glob.glob(os.path.join(new_path, '*DV.txt'))
 
             #read labels and create line ,label file
-
+            if os.path.exists(transcription):
+                transcription = open(transcription,"r")
+                trans_lines = transcription.readlines()
+                next=1
+                max = 0
+                for line in trans_lines:
+                    start, end,turn= line.split('\t')
+                    start2, end2, turn2 = trans_lines[next].split('\t')
+                    if int(start2)<int(end):
+                        if int(end)-int(start2) > max:
+                            max = int(end)-int(start2)
+                            max_start=start
+                        next+=1
+                        if(next==len(trans_lines)-1):
+                            break
+                print(i,max,max_start)
     def create_transcriptions(self, directory):
         path = os.path.join(directory, "Sessions")
-        for i in range(1, 129):
+        for i in range(1, 15):
             new_path = os.path.join(path, str(i))
             transcript = glob.glob(os.path.join(new_path,
                                                  'alignedTranscript_*.txt'))
             word_level_user = glob.glob(os.path.join(new_path,
-                                                     'wordLevel*user'))
+                                                     'wordLevel*user*'))
             word_level_operator = glob.glob(os.path.join(
-                new_path, 'wordLevel*operator'))
+                new_path, 'wordLevel*operator*'))
 
             arousal_files = glob.glob(os.path.join(new_path, '*DA.txt'))
             valence_files = glob.glob(os.path.join(new_path, '*DV.txt'))
+
 
             if not transcript == []:
                 trans = open(os.path.join(new_path, "transcription.txt"), "w")
@@ -43,51 +59,84 @@ class SemaineDataset(Dataset):
                 t_lines = transcript.readlines()
                 user_lines = word_user.readlines()
                 operator_lines = word_operator.readlines()
-                counter_user_lines = 1
-                counter_operator_lines = 1
+                counter_user_lines = 0
+                counter_operator_lines = 0
 
                 for line in t_lines:
+                    new=True
                     if line.split("\t")[0].split(" ")[2] == "User":
                         if counter_user_lines < len(user_lines):
-                            if user_lines[counter_user_lines][0] == ".":
-                                counter_user_lines += 2
-                            else:
-                                time_start = user_lines[
-                                    counter_user_lines].split(" ")[0]
+                            if user_lines[counter_user_lines][0]=="-":
+                                counter_user_lines += 1
 
-                                while counter_user_lines<len(user_lines) and not (\
-                                        user_lines[counter_user_lines][0] == "-"):
-
-                                    if user_lines[counter_user_lines][0] == ".":
-                                        pass
-                                    else:
+                            while not user_lines[counter_user_lines][
+                                          0]=="-":
+                                if new and user_lines[
+                                    counter_user_lines][0] ==".":
+                                    time_start="empty"
+                                    time_end = "empty"
+                                    print("empty")
+                                    new=False
+                                    
+                                elif new and not user_lines[ 
+                                                     counter_user_lines][
+                                                     0] ==".":
+                                    time_start=user_lines[
+                                        counter_user_lines].split(" ")[0]
+                                    time_end = user_lines[
+                                        counter_user_lines].split(" ")[1]
+                                    new = False
+                                
+                                else:
+                                    if not user_lines[counter_user_lines][
+                                        0] == ".":
                                         time_end = user_lines[
-                                            counter_user_lines].split(" ")[1]
-                                    counter_user_lines += 1
-                                counter_user_lines+=1
-
+                                            counter_user_lines].split(" ")[
+                                            1]
+                                        
+                                counter_user_lines += 1
+                                if counter_user_lines==len(user_lines):
+                                    break
+                            trans.write((time_start + "\t" + time_end + "\t" +
+                                         line.split(
+                                             '\t')[1]))                                            
                     else:
                         if counter_operator_lines < len(operator_lines):
-                            if operator_lines[counter_operator_lines][0] == ".":
-                                counter_operator_lines += 2
-                            else:
-                                time_start = operator_lines[
-                                    counter_operator_lines].split(" ")[0]
-
-                                while counter_operator_lines<len(
-                                    operator_lines) and (not operator_lines[counter_operator_lines][0] \
-                                          == "-"):
-                                    if operator_lines[counter_operator_lines][0] == ".":
-                                        pass
-                                    else:
-                                        time_end = operator_lines[
-                                            counter_operator_lines].split(" ")[1]
-                                    counter_operator_lines += 1
-
+                            if operator_lines[counter_operator_lines][0] == "-":
                                 counter_operator_lines += 1
 
-                    trans.write((time_start +"\t"+time_end+"\t" +line.split(
-                        '\t')[1]))
+                            while not operator_lines[counter_operator_lines][
+                                          0] == "-":
+                                if new and operator_lines[
+                                    counter_operator_lines][0] == ".":
+                                    time_start = "empty"
+                                    time_end = "empty"
+                                    print("empty")
+                                    new = False
+
+                                elif new and not operator_lines[
+                                                     counter_operator_lines][
+                                                     0] == ".":
+                                    time_start = operator_lines[
+                                        counter_operator_lines].split(" ")[0]
+                                    time_end = operator_lines[
+                                        counter_operator_lines].split(" ")[1]
+                                    new = False
+
+                                else:
+                                    if not operator_lines[counter_operator_lines][
+                                               0] == ".":
+                                        time_end = operator_lines[
+                                            counter_operator_lines].split(" ")[
+                                            1]
+
+                                counter_operator_lines += 1
+                                if counter_operator_lines == len(
+                                        operator_lines):
+                                    break
+                            trans.write((time_start + "\t" + time_end + "\t" +
+                                         line.split(
+                                             '\t')[1]))
 
                 trans.close()
                 transcript.close()
@@ -170,5 +219,57 @@ class SemaineDataset(Dataset):
 
         return s1, s2, s3
 
-if __name__=='__main__':
-    SemaineDataset("./data/semaine-database_download_2020-01-21_11_41_49")
+
+from torch.utils.data import Dataset
+import glob
+import os
+import re
+
+
+class SemaineDatasetTriplesOnly(Dataset):
+    def __init__(self, directory, transforms=None, train=True):
+
+        self.triples = self.create_triples(directory)
+        self.transforms = transforms
+
+    def create_triples(self, directory):
+        path = os.path.join(directory, "Sessions")
+        triples=[]
+        for i in range(1, 129):
+            new_path = os.path.join(path, str(i))
+            transcript = glob.glob(os.path.join(new_path,"aligned*.txt"))
+            if not transcript == []:
+                trans = open(transcript[0], "r")
+                trans_lines = trans.readlines()
+
+                for i in range(len(trans_lines)-2):
+                    u1 = trans_lines[i].strip().split("\t")[1]
+                    u2 = trans_lines[i+1].strip().split("\t")[1]
+                    u3 = trans_lines[i+2].strip().split("\t")[1]
+                    triples.append([u1,u2,u3])
+
+        return triples
+
+    def map(self, t):
+        self.transforms.append(t)
+        return self
+
+    def __len__(self):
+        return len(self.triples)
+
+    def __getitem__(self, idx):
+        s1, s2, s3 = self.triples[idx]
+        if self.transforms is not None:
+            for t in self.transforms:
+                s1 = t(s1)
+                s2 = t(s2)
+                s3 = t(s3)
+
+        return s1, s2, s3
+
+
+if __name__ == '__main__':
+    dataset = SemaineDatasetTriplesOnly(
+        "./data/semaine-database_download_2020-01-21_11_41_49",)
+    print(dataset[190])
+    print(len(dataset))
