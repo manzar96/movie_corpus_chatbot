158e623e327530191181392b95fc62d55dc5d15f
manzar96 <manoszar96@gmail.com>
2020-02-14 17:16:25 +0200

    implemented perplexity

--- slp/modules/loss.py
+++ slp/modules/loss.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+from ignite.metrics import Loss
+import torch
 
 
 class SequenceCrossEntropyLoss(nn.Module):
@@ -10,3 +12,13 @@ class SequenceCrossEntropyLoss(nn.Module):
         y_pred = y_pred.contiguous().view(-1, y_pred.size(-1))
         targets = targets.contiguous().view(-1)
         return self.criterion(y_pred, targets)
+
+class Perplexity(Loss):
+    def __init__(self, pad_idx=0):
+        super(Perplexity, self).__init__()
+        self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+
+    def forward(self, y_pred, targets):
+        y_pred = y_pred.contiguous().view(-1, y_pred.size(-1))
+        targets = targets.contiguous().view(-1)
+        return torch.exp(self.criterion(y_pred, targets))
