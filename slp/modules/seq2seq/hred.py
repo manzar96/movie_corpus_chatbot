73a7628b7c8120b3270a4c80dd9d1cd39b597e22
manzar96 <manoszar96@gmail.com>
2020-03-07 00:01:15 +0200

    implemented greedy search hred and decoder too

--- slp/modules/seq2seq/hred.py
+++ slp/modules/seq2seq/hred.py
@@ -305,6 +305,7 @@ class HREDDecoder(nn.Module):
         del decoder_outputs
         return dec_output
 
+
 class HRED(nn.Module):
     def __init__(self, options, emb_size, vocab_size, enc_embeddings,
                  dec_embeddings, sos_index, device):
@@ -412,23 +413,16 @@ class HRED(nn.Module):
         return dec_out
 
 
-class GreedySearchHREDDecoder(nn.Module):
+class GreedySearchHRED(nn.Module):
     def __init__(self, hred, device):
-        super(GreedySearchHREDDecoder, self).__init__()
-        self.utt_encoder = hred.enc
-        self.ses_encoder = hred.cont_enc
-        self.decoder = hred.dec
+        super(GreedySearchHRED, self).__init__()
 
         self.enc = hred.enc
-
         self.cont_enc = hred.cont_enc
-
-        self.dec = hred.dec
-
+        self.dec = GreedySearchHREDDecoder(hred.dec, device)
         self.batch_first = hred.batch_first
         self.options = hred.options
         self.sos_index = hred.sos_index
-
 
         # we use a linear layer and tanh act function to initialize the
         # hidden of the decoder.
@@ -440,31 +434,111 @@ class GreedySearchHREDDecoder(nn.Module):
 
         self.device = device
 
-    def forward(self, input_seq, input_length, max_length):
+    def forward(self, input_seq1, input_length1, input_seq2, input_length2):
 
-        # Forward input through encoder model
-        encoder_outputs, encoder_hidden = self.enc(input_seq, input_length)
+        _, hidden1 = self.enc(input_seq1, input_length1)
+        _, hidden2 = self.enc(input_seq2, input_length2)
 
-        # Prepare encoder's final hidden layer to be first hidden input to the decoder
-        decoder_hidden = encoder_hidden[:self.decoder.num_layers]
-        # Initialize decoder input with SOS_token
-        decoder_input = torch.ones(1, 1, device=self.device, dtype=torch.long)\
-                        * SOS_token
-        # Initialize tensors to append decoded words to
-        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
-        all_scores = torch.zeros([0], device=device)
-        # Iteratively decode one word token at a time
-        for _ in range(max_length):
-            # Forward pass through decoder
-            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
-            # Obtain most likely word token and its softmax score
-            current_output = torch.squeeze(decoder_output, dim=1)
-            decoder_output = F.softmax(current_output, dim=1)
-            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
-            # Record token and score
-            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+        """
+               we take the last layer of the hidden state!
+               (Supposing it is a gru)
+               """
+        if self.options.enc_bidirectional:
+            hidden1 = hidden1[-2:]
+            hidden2 = hidden2[-2:]
+        else:
+            hidden1 = hidden1[-1]
+            hidden2 = hidden2[-1]
+
+        hidden1 = hidden1.unsqueeze(dim=1)
+        hidden2 = hidden2.unsqueeze(dim=1)
+        context_input = torch.cat((hidden1, hidden2), dim=1)
+
+        _, contenc_hidden = self.cont_enc(context_input)
+
+        """
+        we take the last layer of the hidden state!
+        (Supposing it is a gru)
+        """
+        if self.options.contenc_bidirectional:
+            contenc_hidden = contenc_hidden[-2:]
+        else:
+            contenc_hidden = contenc_hidden[-1]
+
+        dec_init_hidden = self.tanh(self.cont_enc_to_dec(contenc_hidden))
+
+        dec_init_hidden = dec_init_hidden.view(self.options.dec_num_layers,
+                                               1,
+                                               self.options.dec_hidden_size)
+        # edw isws thelei contiguous!!!
+
+        # edw ftiaxnw to input (nomizw einai midenika alla sto paper vazei 1)
+        # decoder_input = [self.sos_index for _ in range(u3.shape[0])]
+        # decoder_input = torch.tensor(decoder_input).long()
+        # decoder_input = decoder_input.unsqueeze(dim=1)
+        # decoder_input = decoder_input.to(self.device)
+
+        decoder_input = torch.zeros(1, 1).long()
+        decoder_input = decoder_input.to(self.device)
+        dec_tokens, dec_scores = self.dec(decoder_input, dec_init_hidden)
+        return dec_tokens, dec_scores
+
+
+class GreedySearchHREDDecoder(nn.Module):
+    def __init__(self, decoder, device):
+        super(GreedySearchHREDDecoder, self).__init__()
+
+        self.dec = decoder
+        self.max_length = 11
+        self.device = device
+
+    def forward(self, dec_input, dec_hidden=None, enc_output=None):
+        """
+        dec_hidden is used for decoder's hidden state initialization!
+        Usually the encoder's last (from the last timestep) hidden state is
+        passed to decoder's hidden state.
+        enc_output: argument is passed if we want to have attention (it is
+        used only for attention, if you don't want to have attention on your
+        model leave it as is!)
+        dec_lengths: during decoding lengths is not mandatory. however we
+        pass this argument because word rnn receives as input the lengths of
+        input too. (We cannot skip giving lengths because in another
+        situations where samples are padded we want to receive the last
+        unpadded element for every sample in the batch and not the one for
+        t=seq_len)
+        """
+        context_encoded = dec_hidden
+        decoder_outputs = []
+
+        all_tokens = torch.zeros([0], device=self.device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=self.device)
+
+        for i in range(0, self.max_length):
+
+            input_embed = self.dec.embed_in(dec_input)
+            if enc_output is None:
+                dec_out, dec_hidden = self.dec.rnn(input_embed,
+                                                   hx=dec_hidden)
+            else:
+                assert False, "Attention is not implemented"
+
+            # ω(dm,n−1, wm,n−1) = Ho dm,n−1 + Eo wm,n−1 + bo   (olo auto se
+            # diastasi emb_size*2
+            emb_inf_vec = self.dec.emb_to_emb2(input_embed).squeeze(dim=1)
+            dec_inf_vec = self.dec.dec_to_emb2(dec_out).squeeze(dim=1)
+            cont_inf_vec = self.dec.cont_to_emb2(context_encoded).squeeze(
+                dim=0)
+
+            total_out = dec_inf_vec + cont_inf_vec + emb_inf_vec
+
+            #after max_out total_out dims:  emb_size
+            total_out = self.dec.max_out(total_out)
+            out = self.dec.embed_out(total_out)
+
+            out = F.softmax(out, dim=1)
+            decoder_scores, dec_input = torch.max(out, dim=1)
+            all_tokens = torch.cat((all_tokens, dec_input), dim=0)
             all_scores = torch.cat((all_scores, decoder_scores), dim=0)
-            # Prepare current token to be next decoder input (add a dimension)
-            decoder_input = torch.unsqueeze(decoder_input, 0)
-        # Return collections of word tokens and scores
+            dec_input = torch.unsqueeze(dec_input, 0)
+
         return all_tokens, all_scores
