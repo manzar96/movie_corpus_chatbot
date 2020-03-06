e47ff0ca0167339eac7f14f460131f8c32c7aea0
manzar96 <manoszar96@gmail.com>
2020-03-06 21:58:23 +0200

    made input interaction file

--- examples/input_interaction.py
+++ examples/input_interaction.py
@@ -101,105 +101,106 @@ def input_interaction(modeloptions, embfile, emb_dim, checkpointfile,
     pad_index = word2idx[HRED_SPECIAL_TOKENS.PAD.value]
     sos_index = word2idx[HRED_SPECIAL_TOKENS.SOU.value]
     eos_index = word2idx[HRED_SPECIAL_TOKENS.EOU.value]
-
+    print("Loaded Embeddings...")
     #  --- load model using loaded embeddings ---
     model = create_model(modeloptions, embeddings, emb_dim, vocab_size,
                          sos_index, device)
     model = from_checkpoint(checkpointfile, model, map_location='cpu')
     model = model.to(device)
-
+    print("Loaded Model...")
     # --- create searcher for encoding user's input and for providing an
     # answer ---
     searcher = create_searcher(model)
     searcher.eval()
+    print("Interacting...")
     evaluate_input(searcher, word2idx, idx2word, device)
 
 
 if __name__ == '__main__':
 
     # --- fix argument parser default values --
-    parser1 = argparse.ArgumentParser(description='HRED parameter options')
-    parser1.add_argument('-n', dest='name', help='enter suffix for model files')
-    parser1.add_argument('-model_path', dest='model_path', default='./models', help='enter the path in which you want to store the model state')
-    parser1.add_argument('-enchidden', dest='enc_hidden_size',
+    parser = argparse.ArgumentParser(description='HRED parameter options and'
+                                                 'checkpoints')
+
+    parser.add_argument('--ckpt', type=str, help='Model checkpoint')
+    parser.add_argument('--embeddings', type=str, help='Embeddings file')
+    parser.add_argument('--emb_dim', type=int, help='Embeddings dimension')
+
+    parser.add_argument('--output', type=str, help='Output file')
+    parser.add_argument('--device', type=str, help='Device cpu|cuda:X')
+
+    parser.add_argument('-n', dest='name', default='hred', help='enter '
+                                                                 'suffix for ' \
+                                                      'model files')
+    parser.add_argument('-model_path', dest='model_path', default='./models', help='enter the path in which you want to store the model state')
+    parser.add_argument('-enchidden', dest='enc_hidden_size',
                         action='store_true',
                         default=256, help='encoder hidden size')
-    parser1.add_argument('-embdrop', dest='embeddings_dropout',
+    parser.add_argument('-embdrop', dest='embeddings_dropout',
                         action='store_true',
                         default=0, help='embeddings dropout')
-    parser1.add_argument('-encembtrain', dest='enc_finetune_embeddings',
+    parser.add_argument('-encembtrain', dest='enc_finetune_embeddings',
                         action='store_true',
                         default=False, help='encoder finetune embeddings')
-    parser1.add_argument('-encnumlayers', dest='enc_num_layers',
+    parser.add_argument('-encnumlayers', dest='enc_num_layers',
                         action='store_true',
                         default=1, help='encoder number of layers')
-    parser1.add_argument('-encbi', dest='enc_bidirectional',
+    parser.add_argument('-encbi', dest='enc_bidirectional',
                         action='store_true',
                         default=False, help='bidirectional enc')
-    parser1.add_argument('-encdrop', dest='enc_dropout', action='store_true',
+    parser.add_argument('-encdrop', dest='enc_dropout', action='store_true',
                         default=0, help='encoder dropout')
 
-    parser1.add_argument('-continputsize', dest='contenc_input_size',
+    parser.add_argument('-continputsize', dest='contenc_input_size',
                         action='store_true',
                         default=256, help='context encoder input size')
-    parser1.add_argument('-conthiddensize', dest='contenc_hidden_size',
+    parser.add_argument('-conthiddensize', dest='contenc_hidden_size',
                         action='store_true',
                         default=256, help='context encoder hidden size')
-    parser1.add_argument('-contnumlayers', dest='contenc_num_layers',
+    parser.add_argument('-contnumlayers', dest='contenc_num_layers',
                         action='store_true',
                         default=1, help='context encoder number of layers')
-    parser1.add_argument('-contencdrop', dest='contenc_dropout',
+    parser.add_argument('-contencdrop', dest='contenc_dropout',
                         action='store_true',
                         default=0, help='context encoder dropout')
-    parser1.add_argument('-contencbi', dest='contenc_bidirectional',
+    parser.add_argument('-contencbi', dest='contenc_bidirectional',
                         action='store_true',
                         default=False, help='bidirectional enc')
-    parser1.add_argument('-contenctype', dest='contenc_rnn_type',
+    parser.add_argument('-contenctype', dest='contenc_rnn_type',
                         action='store_true',
                         default='gru', help='bidirectional enc')
 
-    parser1.add_argument('-dechidden', dest='dec_hidden_size',
+    parser.add_argument('-dechidden', dest='dec_hidden_size',
                         action='store_true',
                         default=256, help='decoder hidden size')
-    parser1.add_argument('-decembtrain', dest='dec_finetune_embeddings',
+    parser.add_argument('-decembtrain', dest='dec_finetune_embeddings',
                         action='store_true',
                         default=False, help='decoder finetune embeddings')
-    parser1.add_argument('-decnumlayers', dest='dec_num_layers',
+    parser.add_argument('-decnumlayers', dest='dec_num_layers',
                         action='store_true',
                         default=1, help='decoder number of layers')
-    parser1.add_argument('-decbi', dest='dec_bidirectional',
+    parser.add_argument('-decbi', dest='dec_bidirectional',
                         action='store_true',
                         default=False, help='bidirectional decoder')
-    parser1.add_argument('-decdrop', dest='dec_dropout', action='store_true',
+    parser.add_argument('-decdrop', dest='dec_dropout', action='store_true',
                         default=0, help='decoder dropout')
-    parser1.add_argument('-decmergebi', dest='dec_merge_bi',
+    parser.add_argument('-decmergebi', dest='dec_merge_bi',
                         action='store_true',
                         default='cat', help='decoder merge bidirectional '
                                        'method')
-    parser1.add_argument('-dectype', dest='dec_rnn_type',
+    parser.add_argument('-dectype', dest='dec_rnn_type',
                         action='store_true',
                         default='gru', help='decoder rnn type')
 
-    parser1.add_argument('-bf', dest='batch_first', action='store_true',
+    parser.add_argument('-bf', dest='batch_first', action='store_true',
                         default=True, help='batch first')
-    parser1.add_argument('-tf', dest='teacherforcing_ratio',
+    parser.add_argument('-tf', dest='teacherforcing_ratio',
                         action='store_true',
                         default=1., help='teacher forcing ratio')
 
-    model_options = parser1.parse_args()
+    options = parser.parse_args()
 
-    parser2 = argparse.ArgumentParser()
-
-    parser2.add_argument('--ckpt', type=str, help='Model checkpoint')
-    parser2.add_argument('--embeddings', type=str, help='Embeddings file')
-    parser2.add_argument('--emb_dim', type=int, help='Embeddings dimension')
-
-    parser2.add_argument('--output', type=str, help='Output file')
-    parser2.add_argument('--device', type=str, help='Device cpu|cuda:X')
-
-    options = parser2.parse_args()
-
-    input_interaction(model_options,
+    input_interaction(options,
                       options.embeddings,
                       options.emb_dim,
                       options.ckpt,
