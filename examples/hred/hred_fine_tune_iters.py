146c076bd0910ffff7a494dc6822b906f59c29c3
manzar96 <manoszar96@gmail.com>
2020-03-19 22:26:41 +0200

    training and input interaction for the fine tuned model

---
+++ examples/hred/hred_fine_tune_iters.py
@@ -0,0 +1,245 @@
+import os
+import torch
+import argparse
+from torch.optim import Adam
+
+from slp.config.special_tokens import HRED_SPECIAL_TOKENS
+from slp.data.utils import train_test_split
+from slp.data.transforms import DialogSpacyTokenizer, ToTokenIds, ToTensor
+from slp.data.collators import HRED_Collator
+from slp.util.embeddings import EmbeddingsLoader, create_emb_file
+
+from slp.modules.loss import SequenceCrossEntropyLoss, Perplexity
+from slp.modules.seq2seq.hredseq2seq import HREDSeq2Seq
+from slp.trainer.trainer import HREDIterationsTrainer
+
+from slp.util import from_checkpoint
+
+from slp.data.Semaine import SemaineDatasetTriplesOnly
+from slp.data.moviecorpus import MovieCorpusDatasetv2
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(DEVICE)
+BATCH_TRAIN_SIZE = 64
+BATCH_VAL_SIZE = 34
+
+
+
+def load_trainer_factory(options, emb_dim, vocab_size, embeddings, pad_index,
+                    sos_index,loadfrom, checkpoint_dir=None, device=DEVICE):
+
+    model = HREDSeq2Seq(options, emb_dim, vocab_size, embeddings, embeddings,
+                        sos_index, device)
+    import ipdb;ipdb.set_trace()
+    model = from_checkpoint(loadfrom, model, map_location='cpu')
+    model = model.to(device)
+
+    numparams = sum([p.numel() for p in model.parameters() if p.requires_grad])
+    print('Trainable Parameters: {}'.format(numparams))
+
+    optimizer = Adam(
+        [p for p in model.parameters() if p.requires_grad],
+        lr=1e-3, weight_decay=1e-6)
+
+    criterion = SequenceCrossEntropyLoss(pad_index)
+    perplexity = Perplexity(pad_index)
+    metrics = [perplexity]
+
+    trainer = HREDIterationsTrainer(model, optimizer, criterion, metrics,
+                                    checkpoint_dir=checkpoint_dir,
+                                    save_every=options.save_every,
+                                    print_every=options.print_every,
+                                    device=device)
+    return trainer
+
+
+if __name__ == '__main__':
+
+    # --- fix argument parser default values --
+    parser = argparse.ArgumentParser(description='HRED parameter options')
+
+    parser.add_argument('--ckpt', type=str, help='Model checkpoint')
+    parser.add_argument('--lckpt', type=str, help='Load model from checkpoint')
+    parser.add_argument('--embeddings', type=str, help='Embeddings file')
+    parser.add_argument('--emb_dim', type=int, help='Embeddings dimension')
+
+    parser.add_argument('-enchidden', dest='enc_hidden_size', type=int,
+                        default=256, help='encoder hidden size')
+    parser.add_argument('-embdrop', dest='embeddings_dropout', type=float,
+                        default=0, help='embeddings dropout')
+    parser.add_argument('-encembtrain', dest='enc_finetune_embeddings',
+                        action='store_true', default=False,
+                        help='encoder finetune embeddings')
+    parser.add_argument('-encnumlayers', dest='enc_num_layers', type=int,
+                        default=1, help='encoder number of layers')
+    parser.add_argument('-encbi', dest='enc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-encdrop', dest='enc_dropout', type=float,
+                        default=0, help='encoder dropout')
+
+    parser.add_argument('-continputsize', dest='contenc_input_size',
+                        type=int,
+                        default=256, help='context encoder input size')
+    parser.add_argument('-conthiddensize', dest='contenc_hidden_size',
+                        type=int,
+                        default=256, help='context encoder hidden size')
+    parser.add_argument('-contnumlayers', dest='contenc_num_layers',
+                        type=int,
+                        default=1, help='context encoder number of layers')
+    parser.add_argument('-contencdrop', dest='contenc_dropout',
+                        type=float,
+                        default=0, help='context encoder dropout')
+    parser.add_argument('-contencbi', dest='contenc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-contenctype', dest='contenc_rnn_type',
+                        action='store_true',
+                        default='gru', help='bidirectional enc')
+
+    parser.add_argument('-dechidden', dest='dec_hidden_size',
+                        type=int,
+                        default=256, help='decoder hidden size')
+    parser.add_argument('-decembtrain', dest='dec_finetune_embeddings',
+                        action='store_true',
+                        default=False, help='decoder finetune embeddings')
+    parser.add_argument('-decnumlayers', dest='dec_num_layers',
+                        type=int,
+                        default=1, help='decoder number of layers')
+    parser.add_argument('-decbi', dest='dec_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional decoder')
+    parser.add_argument('-decdrop', dest='dec_dropout', type=float,
+                        default=0, help='decoder dropout')
+    parser.add_argument('-decmergebi', dest='dec_merge_bi',
+                        action='store_true',
+                        default='cat', help='decoder merge bidirectional '
+                                            'method')
+    parser.add_argument('-dectype', dest='dec_rnn_type',
+                        action='store_true',
+                        default='gru', help='decoder rnn type')
+
+    parser.add_argument('-bf', dest='batch_first', action='store_true',
+                        default=True, help='batch first')
+    parser.add_argument('-tf', dest='teacherforcing_ratio',
+                        action='store_true',
+                        default=1., help='teacher forcing ratio')
+    parser.add_argument('-pt', dest='pretraining',
+                        action='store_true',
+                        default=False, help='Pretraining model (only encoder'
+                                            'decoder)')
+    parser.add_argument('-sl', dest='samplelimit', type=int,
+                        default=None, help='sample limit used for training')
+    parser.add_argument('-iters', type=int,
+                        default=1000, help='iterations for training in mini '
+                                           'batches')
+    parser.add_argument('-save_every', type=int,
+                        default=100, help='save every X iters')
+    parser.add_argument('-print_every', type=int,
+                        default=100, help='print every X iters')
+    parser.add_argument('-shared', action='store_true',
+                        default=False, help='shared weights between encoder '
+                                            'and decoder')
+    parser.add_argument('-shared_emb', action='store_true',
+                        default=False, help='shared embedding layer')
+
+    options = parser.parse_args()
+
+    # ---  read data to create vocabulary dict ---
+
+    tokenizer = DialogSpacyTokenizer(lower=True, prepend_sos=True,
+                                     append_eos=True,
+                                     specials=HRED_SPECIAL_TOKENS)
+
+    dataset = SemaineDatasetTriplesOnly(
+        "./data/semaine-database_download_2020-01-21_11_41_49")
+    vocab_dict = dataset.create_vocab_dict(tokenizer)
+
+    # --- create new embedding file ---
+
+    new_emb_file = './cache/new_embs.txt'
+    old_emb_file = options.embeddings
+    freq_words_file = './cache/freq_words.txt'
+    emb_dim = options.emb_dim
+
+    create_emb_file(new_emb_file, old_emb_file, freq_words_file, vocab_dict,
+                    most_freq=10000)
+
+    # --- load new embeddings! ---
+    word2idx, idx2word, embeddings = EmbeddingsLoader(new_emb_file, emb_dim,
+                                                      extra_tokens=
+                                                      HRED_SPECIAL_TOKENS
+                                                      ).load()
+    vocab_size = len(word2idx)
+    print("Vocabulary size: {}".format(vocab_size))
+
+    # --- read dataset again and apply transforms ---
+    to_token_ids = ToTokenIds(word2idx, specials=HRED_SPECIAL_TOKENS)
+    to_tensor = ToTensor()
+
+    dataset = SemaineDatasetTriplesOnly(
+        './data/semaine-database_download_2020-01-21_11_41_49', transforms=[
+            tokenizer, to_token_ids, to_tensor])
+
+    print("Dataset size: {}".format(len(dataset)))
+    # --- make train and val loaders ---
+
+    collator_fn = HRED_Collator(device='cpu')
+    train_loader, val_loader = train_test_split(dataset,
+                                                batch_train=BATCH_TRAIN_SIZE,
+                                                batch_val=BATCH_VAL_SIZE,
+                                                collator_fn=collator_fn,
+                                                test_size=0.2)
+
+    pad_index = word2idx[HRED_SPECIAL_TOKENS.PAD.value]
+    sos_index = word2idx[HRED_SPECIAL_TOKENS.SOS.value]
+    eos_index = word2idx[HRED_SPECIAL_TOKENS.EOS.value]
+    unk_index = word2idx[HRED_SPECIAL_TOKENS.UNK.value]
+    print("sos index {}".format(sos_index))
+    print("eos index {}".format(eos_index))
+    print("pad index {}".format(pad_index))
+    print("unk index {}".format(unk_index))
+
+    # --- make model and train it ---
+
+    checkpoint_dir = options.ckpt
+    info_dir = os.path.join(checkpoint_dir, "info.txt")
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    with open(info_dir, "w") as info:
+        info.write("DATA USED INFO\n")
+        info.write("Data samples: {} \n".format(len(dataset)))
+        info.write("Vocabulary size: {} \n".format(vocab_size))
+
+        info.write("MODEL's INFO\n")
+        info.write("Utterance Encoder: {} layers, {} bidirectional, "
+                   "{} dropout , "
+                   "{} hidden \n".format(options.enc_num_layers,
+                                         options.enc_bidirectional,
+                                         options.enc_dropout,
+                                         options.enc_hidden_size))
+
+        info.write("Context Encoder: {} layers, {} bidirectional, {} dropout , "
+                   "{} hidden , {} input \n".format(options.contenc_num_layers,
+                                                    options.contenc_bidirectional,
+                                                    options.contenc_dropout,
+                                                    options.contenc_hidden_size,
+                                                    options.contenc_input_size))
+
+        info.write("Decoder: {} layers, {} bidirectional, {} dropout , "
+                   "{} hidden \n".format(options.dec_num_layers,
+                                         options.dec_bidirectional,
+                                         options.dec_dropout,
+                                         options.dec_hidden_size))
+
+        info.write("More info: \n")
+
+        info.close()
+
+    trainer = load_trainer_factory(options, emb_dim, vocab_size, embeddings,
+                              pad_index, sos_index,options.lckpt,
+                                   checkpoint_dir,
+                              device=DEVICE)
+
+    trainer.fit(train_loader, val_loader, n_iters=options.iters)
---
+++ examples/hred/input_interaction_fine_tuned.py
@@ -0,0 +1,241 @@
+import torch
+import argparse
+from slp.data.transforms import SpacyTokenizer, ToTokenIds, ToTensor
+
+from slp.util import from_checkpoint
+from slp.util.embeddings import EmbeddingsLoader
+from slp.config.special_tokens import HRED_SPECIAL_TOKENS
+from slp.modules.seq2seq.hredseq2seq import HREDSeq2Seq, GreedySearchHREDSeq2Seq
+
+import unicodedata
+import re
+
+def unicodeToAscii( s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+# Lowercase, trim, and remove non-letter characters
+def normalizeString( s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+
+def create_model(modeloptions, embeddings, emb_dim, vocab_size, sos_index,
+                 device):
+
+    model = HREDSeq2Seq(modeloptions, emb_dim, vocab_size, embeddings,
+                 embeddings, sos_index, device)
+    return model
+
+
+def create_searcher(model, device):
+    searcher = GreedySearchHREDSeq2Seq(model, device)
+    return searcher
+
+
+def load_embeddings(emb_file, emb_dim):
+    loader = EmbeddingsLoader(emb_file, emb_dim, extra_tokens=HRED_SPECIAL_TOKENS)
+    word2idx, idx2word, embeddings = loader.load()
+    return word2idx, idx2word, embeddings
+
+
+# def load_model_from_checkpoint(embfile, checkpointfile,device):
+#
+#     word2idx, idx2word, embeddings = load_embeddings(embfile)
+#
+#     model = create_model(embeddings)
+#     model = from_checkpoint(checkpointfile, model, map_location='cpu')
+#     model = model.to(device)
+#
+#     return model
+
+def evaluate(searcher, idx2word, previous, sentence1, device):
+
+    if previous is None:
+        indexes_batch = sentence1
+        input_batch = torch.unsqueeze(indexes_batch, 0)
+        lengths = torch.tensor([len(indexes) for indexes in input_batch])
+        input_batch1 = input_batch.to(device)
+        lengths1 = lengths.to(device)
+
+        empty_input = torch.zeros(input_batch1.shape[0], input_batch1.shape[1],
+                                  device=device)
+        len_empty = torch.tensor([len(s) for s in empty_input],
+                                 device=device)
+        # Decode sentence with searcher
+        tokens, scores = searcher(empty_input, len_empty, input_batch1, lengths1)
+        decoded_words = [idx2word[token.item()] for token in tokens]
+    else:
+        indexes_batch = previous
+        input_batch = torch.unsqueeze(indexes_batch, 0)
+        lengths = torch.tensor([len(indexes) for indexes in input_batch])
+        input_batch1 = input_batch.to(device)
+        lengths1 = lengths.to(device)
+
+        indexes_batch = sentence1
+        input_batch = torch.unsqueeze(indexes_batch, 0)
+        lengths = torch.tensor([len(indexes) for indexes in input_batch])
+        input_batch2 = input_batch.to(device)
+        lengths2 = lengths.to(device)
+
+        # Decode sentence with searcher
+        tokens, scores = searcher(input_batch1, lengths1, input_batch2,
+                                  lengths2)
+        decoded_words = [idx2word[token.item()] for token in tokens]
+
+    return decoded_words
+
+
+def evaluate_input(searcher, word2idx, idx2word, device):
+    tokenizer = SpacyTokenizer(specials=HRED_SPECIAL_TOKENS)
+    to_token_ids = ToTokenIds(word2idx, specials=HRED_SPECIAL_TOKENS)
+    to_tensor = ToTensor()
+    transforms = [tokenizer, to_token_ids, to_tensor]
+    previous = None
+    while True:
+        try:
+            # Get input sentence
+            input_sentence1 = input('> ')
+            if input_sentence1 == 'q' or input_sentence1 == 'quit': break
+
+            # Normalize sentence
+            input_sentence1 = normalizeString(input_sentence1)
+
+            # Evaluate sentence
+            for t in transforms:
+                input_sentence1 = t(input_sentence1)
+
+            output_words = evaluate(searcher, idx2word, previous,
+                                    input_sentence1, device)
+            previous = input_sentence1
+            print(output_words)
+            output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+            print('Bot:', ' '.join(output_words))
+
+        except KeyError:
+            print("Error: Encountered unknown word.")
+
+
+def input_interaction(modeloptions, embfile, emb_dim, checkpointfile,
+                      outputfile, device):
+
+    word2idx, idx2word, embeddings = load_embeddings(embfile, emb_dim)
+    vocab_size = len(word2idx)
+    pad_index = word2idx[HRED_SPECIAL_TOKENS.PAD.value]
+    sos_index = word2idx[HRED_SPECIAL_TOKENS.SOU.value]
+    eos_index = word2idx[HRED_SPECIAL_TOKENS.EOU.value]
+    print("Loaded Embeddings...")
+    #  --- load model using loaded embeddings ---
+    model = create_model(modeloptions, embeddings, emb_dim, vocab_size,
+                         sos_index, device)
+    model = from_checkpoint(checkpointfile, model, map_location='cpu')
+    model = model.to(device)
+    print("Loaded Model...")
+    # --- create searcher for encoding user's input and for providing an
+    # answer ---
+    searcher = create_searcher(model, device)
+    searcher = searcher.to(device)
+    searcher.eval()
+    print("Interacting...")
+    evaluate_input(searcher, word2idx, idx2word, device)
+
+
+if __name__ == '__main__':
+
+    # --- fix argument parser default values --
+    parser = argparse.ArgumentParser(description='HRED parameter options and'
+                                                 'checkpoints')
+
+    parser.add_argument('--ckpt', type=str, help='Model checkpoint')
+    parser.add_argument('--embeddings', type=str, help='Embeddings file')
+    parser.add_argument('--emb_dim', type=int, help='Embeddings dimension')
+
+    parser.add_argument('--output', type=str, help='Output file')
+    parser.add_argument('--device', type=str, help='Device cpu|cuda:X')
+
+    parser.add_argument('-enchidden', dest='enc_hidden_size', type=int,
+                        default=256, help='encoder hidden size')
+    parser.add_argument('-embdrop', dest='embeddings_dropout', type=float,
+                        default=0, help='embeddings dropout')
+    parser.add_argument('-encembtrain', dest='enc_finetune_embeddings',
+                        action='store_true', default=False,
+                        help='encoder finetune embeddings')
+    parser.add_argument('-encnumlayers', dest='enc_num_layers', type=int,
+                        default=1, help='encoder number of layers')
+    parser.add_argument('-encbi', dest='enc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-encdrop', dest='enc_dropout', type=float,
+                        default=0, help='encoder dropout')
+
+    parser.add_argument('-continputsize', dest='contenc_input_size',
+                        type=int,
+                        default=256, help='context encoder input size')
+    parser.add_argument('-conthiddensize', dest='contenc_hidden_size',
+                        type=int,
+                        default=256, help='context encoder hidden size')
+    parser.add_argument('-contnumlayers', dest='contenc_num_layers',
+                        type=int,
+                        default=1, help='context encoder number of layers')
+    parser.add_argument('-contencdrop', dest='contenc_dropout',
+                        type=float,
+                        default=0, help='context encoder dropout')
+    parser.add_argument('-contencbi', dest='contenc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-contenctype', dest='contenc_rnn_type',
+                        action='store_true',
+                        default='gru', help='bidirectional enc')
+
+    parser.add_argument('-dechidden', dest='dec_hidden_size',
+                        type=int,
+                        default=256, help='decoder hidden size')
+    parser.add_argument('-decembtrain', dest='dec_finetune_embeddings',
+                        action='store_true',
+                        default=False, help='decoder finetune embeddings')
+    parser.add_argument('-decnumlayers', dest='dec_num_layers',
+                        type=int,
+                        default=1, help='decoder number of layers')
+    parser.add_argument('-decbi', dest='dec_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional decoder')
+    parser.add_argument('-decdrop', dest='dec_dropout', type=float,
+                        default=0, help='decoder dropout')
+    parser.add_argument('-decmergebi', dest='dec_merge_bi',
+                        action='store_true',
+                        default='cat', help='decoder merge bidirectional '
+                                            'method')
+    parser.add_argument('-dectype', dest='dec_rnn_type',
+                        action='store_true',
+                        default='gru', help='decoder rnn type')
+
+    parser.add_argument('-bf', dest='batch_first', action='store_true',
+                        default=True, help='batch first')
+    parser.add_argument('-tf', dest='teacherforcing_ratio',
+                        action='store_true',
+                        default=1., help='teacher forcing ratio')
+    parser.add_argument('-pt', dest='pretraining',
+                        action='store_true',
+                        default=False, help='Pretraining model (only encoder'
+                                            'decoder)')
+
+    parser.add_argument('-shared', action='store_true',
+                        default=False, help='shared weights between encoder '
+                                            'and decoder')
+    parser.add_argument('-shared_emb', action='store_true',
+                        default=False, help='shared embedding layer')
+
+    options = parser.parse_args()
+
+    input_interaction(options,
+                      options.embeddings,
+                      options.emb_dim,
+                      options.ckpt,
+                      options.output,
+                      options.device)
