e790a8198b6eb78b356f5bf3ef2a9d1428a116a1
manzar96 <manoszar96@gmail.com>
2020-03-25 15:59:54 +0200

    init randomly embeddings

--- slp/data/vocab.py
+++ slp/data/vocab.py
@@ -16,3 +16,28 @@ def create_vocab(corpus, vocab_size=5000, extra_tokens=None):
         words = words[:vocab_size]
     vocab = dict(zip(words, itertools.count()))
     return vocab
+
+
+def word2idx_from_dataset(wordcounts, most_freq=None, extra_tokens=None):
+    word2idx = {}
+    idx2word = {}
+    counter = 0
+    if extra_tokens is not None:
+        for token in extra_tokens:
+            word2idx[token.value] = counter
+            idx2word[counter] = token.value
+            counter += 1
+    if most_freq is None:
+        for word in wordcounts:
+            if word not in word2idx:
+                word2idx[word] = counter
+                idx2word[counter] = word
+                counter += 1
+    else:
+        sorted_voc = sorted(wordcounts.items(), key=lambda kv: kv[1])
+        for word in sorted_voc[-most_freq:]:
+            if word not in word2idx:
+                word2idx[word] = counter
+                idx2word[counter] = word
+                counter += 1
+    return word2idx, idx2word
