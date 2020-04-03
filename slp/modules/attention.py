d9939d7270bb8535f2a1556ef1494f2a1f2d0d68
manzar96 <manoszar96@gmail.com>
2020-04-04 01:28:00 +0300

    first commit Luong attention

--- slp/modules/attention.py
+++ slp/modules/attention.py
@@ -187,3 +187,44 @@ class MultiheadAttentionParallel(nn.Module):
 
 
 MultiheadAttention = MultiheadAttentionParallel
+
+
+# Luong attention layer
+class LuongAttn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(LuongAttn, self).__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method,
+                             "is not an appropriate attention method.")
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        import ipdb;ipdb.set_trace()
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(encoder_output)
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat(
+            (hidden.expand(encoder_output.size(0), -1, -1), encoder_output),
+            2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # Calculate the attention weights (energies) based on the given method
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
