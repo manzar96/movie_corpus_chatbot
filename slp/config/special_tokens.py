a7a3d7636adb2fca1f3c4bb51b963b0ad804c7f9
manzar96 <manoszar96@gmail.com>
2020-04-04 02:45:42 +0300

    resolve conflicts

--- slp/config/special_tokens.py
+++ slp/config/special_tokens.py
@@ -19,3 +19,32 @@ class SPECIAL_TOKENS(Enum):
     @classmethod
     def to_list(cls):
         return list(map(lambda x: x.value, cls))
+
+
+class HRED_SPECIAL_TOKENS(Enum):
+    """
+    end-of-utterance: </s>
+    end-of-dialogue: </d>
+    first speaker: <first_speaker>
+    second speaker: <second_speaker>
+    third speaker: <third_speaker>
+    minor speaker: <minor_speaker>
+    voice over: <voice_over>
+    off screen: <off_screen>
+    pause: <pause>
+    """
+
+    PAD = '<PAD>'
+    UNK = '<unk>'
+    SOS = '<s>'
+    EOS = '</s>'
+    EOT = '</t>'
+
+    @classmethod
+    def has_token(cls, token):
+        return any(token == t.name or token == t.value
+                   for t in cls)
+
+    @classmethod
+    def to_list(cls):
+        return list(map(lambda x: x.value, cls))
