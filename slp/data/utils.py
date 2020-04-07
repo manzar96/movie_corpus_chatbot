f0cff7cd3199ea2ff09d04a63de8e3f5a26954f6
manzar96 <manoszar96@gmail.com>
2020-04-07 21:46:20 +0300

    make train val test split function

--- slp/data/utils.py
+++ slp/data/utils.py
@@ -1,8 +1,7 @@
 import numpy as np
 import os
 import pickle
-from torch.utils.data import DataLoader, SubsetRandomSampler
-
+from torch.utils.data import DataLoader, SubsetRandomSampler,Subset
 
 def dataloaders_from_indices(dataset, train_indices, val_indices, batch_train,
                              batch_val, collator_fn):
@@ -39,7 +38,7 @@ def train_test_split(dataset, batch_train, batch_val, collator_fn,
     return dataloaders_from_indices(dataset, train_indices, val_indices,
                                     batch_train, batch_val, collator_fn)
 
-def make_train_val_test_pickles(outfolder,dataset,val_size,test_size,\
+def make_train_val_test_split(dataset,val_size,test_size,\
                                          shuffle=True,
                                 seed=None):
     dataset_size = len(dataset)
@@ -53,19 +52,8 @@ def make_train_val_test_pickles(outfolder,dataset,val_size,test_size,\
     train_indexes = indices[val_test_split:]
     test_indexes = indices[:test_split]
     val_indexes = indices[test_split:val_test_split]
-    train_dataset = list(map(dataset.__getitem__, train_indexes))
-    val_dataset = list(map(dataset.__getitem__, val_indexes))
-    test_dataset = list(map(dataset.__getitem__, test_indexes))
-    if not os.path.exists(outfolder):
-        os.makedirs(outfolder)
+    train_list = list(map(dataset.__getitem__,train_indexes))
+    val_list = list(map(dataset.__getitem__,val_indexes))
+    test_list = list(map(dataset.__getitem__,test_indexes))
+    return train_list,val_list,test_list
 
-    with open(os.path.join(outfolder,'train_set.pkl'),'wb') as handle:
-        pickle.dump(train_dataset,handle,protocol=pickle.HIGHEST_PROTOCOL)
-    handle.close()
-    with open(os.path.join(outfolder, 'val_set.pkl'), 'wb') as handle:
-        pickle.dump(val_dataset,handle,protocol=pickle.HIGHEST_PROTOCOL)
-    handle.close()
-
-    with open(os.path.join(outfolder, 'test_set.pkl'), 'wb') as handle:
-        pickle.dump(test_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    handle.close()
